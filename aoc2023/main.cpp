1f9d475960641b85d67ac7c319c9021e71eee1c0
Gyömörey Domonkos <domonkosgyomorey@gmail.com>
2023-12-15 23:16:32 +0100

    Day 1 problem 2 halfly solved, so there is some bug in my code

--- aoc2023/main.cpp
+++ aoc2023/main.cpp
@@ -1,40 +1,93 @@
 #include <iostream>
-#include <numeric>
 #include <string>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 
 std::vector<std::string> read_file(const char* file_path);
 int day1_1(std::vector<std::string> input);
+int day1_2(const std::vector<std::string> input);
 
 int main(int argc, char** argv) {
-	std::cout << "Hello, World!" << std::endl;
-	std::cout<<day1_1(read_file("day1_1.txt"))<<std::endl;
+	std::cout << day1_1(read_file("day1_1.txt")) << std::endl;
+	std::cout<<day1_2(read_file("day1_2.txt"))<<std::endl;
 	return 0;
 }
 
 std::vector<std::string> read_file(const char* file_path) {
 	
-	std::vector<std::string> res;
-	std::ifstream file(file_path);
-	if (!file) {
-		std::cout << "Unable ot open file :" << file_path << std::endl;
+	std::fstream file(file_path);
+
+	if (!file.is_open()) {
+		std::cerr << "Unable to open file." << file_path << std::endl;
 		exit(-1);
 	}
-	std::string str;
-	while (std::getline(file, str)) {
-		res.push_back(str);
+
+	std::vector<std::string> lines;
+	std::string line;
+
+	while (std::getline(file, line)) {
+		lines.push_back(line);
 	}
-	return res;
+
+	file.close();
+	file.clear();
+	file.seekg(0, std::ios::beg);
+	return lines;
 }
 
 int day1_1(const std::vector<std::string> input) {
 	const char* digits = "0123456789";
 	std::size_t sum = 0;
 	for(auto line : input){
-			int first_digit = (line.at(line.find_first_of(digits)) - '0');
-			int last_digit = (line.at(line.find_last_of(digits)) - '0');
+			uint16_t first_digit = (line.at(line.find_first_of(digits)) - '0');
+			uint16_t last_digit = (line.at(line.find_last_of(digits)) - '0');
 			sum += 10*first_digit + last_digit;
 	}
 	return sum;
+}
+
+
+// TODO: Something is wrong somewhere in my logic
+int day1_2(const std::vector<std::string> input) {
+	
+	std::vector<std::string> transformed_input;
+	
+	std::unordered_map<std::string, uint16_t> word_to_digit;
+	// +1, cause word_to_digit["wrong_str"] == 0
+	word_to_digit["zero"] = 1;
+	word_to_digit["one"] = 2;
+	word_to_digit["two"] = 3;
+	word_to_digit["three"] = 4;
+	word_to_digit["four"] = 5;
+	word_to_digit["five"] = 6;
+	word_to_digit["six"] = 7;
+	word_to_digit["seven"] = 8;
+	word_to_digit["eight"] = 9;
+	word_to_digit["nine"] = 10;
+
+	const uint16_t MIN_WORD_LEN = 3;
+	const uint16_t MAX_WORD_LEN = 6;
+
+	// For each row
+	for (auto line : input) {
+		// For each char
+		for (std::size_t i = 0; i < line.length(); i++) {
+			// check the substring from i to j
+			for (int j = MIN_WORD_LEN; j < MAX_WORD_LEN && i + j - 1 < line.length(); j++) {
+				auto val = word_to_digit[line.substr(i, j)];
+				if (val > 0) {
+					// cause the mapping
+					val -= 1;
+
+					line.replace(i, j, std::to_string(val));
+					break;
+				}
+			}
+		}
+		// push the replaced lines into a vector (cause the local line var)
+		transformed_input.push_back(line);
+	}
+
+	return day1_1(transformed_input);
 }
